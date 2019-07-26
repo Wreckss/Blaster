4514c8ea69109f852b7fcc30b62218ecdb6b331b
tacoRiot <46939881+tacoRiot@users.noreply.github.com>
2019-07-26 16:28:57 -0600

    added player sprite

--- Blaster/GameScene.swift
+++ Blaster/GameScene.swift
@@ -10,11 +10,18 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMove(to view: SKView) {
+        let player = SKSpriteNode(imageNamed: "player")
 
         if let particles = SKEmitterNode(fileNamed: "Starfield") {
             particles.position = CGPoint(x: 1000, y: 0)
+            particles.advanceSimulationTime(60) //this fast forwards the generation of the Starfield by 60sec
             particles.zPosition = -1
             addChild(particles)
         }
+        
+        player.name = "player"
+        player.position.x = frame.minX + 75
+        player.zPosition = 1
+        addChild(player)
     }
 }
